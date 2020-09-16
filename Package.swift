9dbd7eb7ee92a94c7d80c9d461d60ccd9372272a
Rita Zerrizuela <zeta@widcket.com>
2020-09-16 13:49:29 -0300

    Restore git-based dependencies

--- Package.swift
+++ Package.swift
@@ -11,16 +11,24 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Auth0",
-            targets: ["Auth0"])
+            targets: ["Auth0Dependencies"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "../SimpleKeychain"),
-        .package(name: "JWTDecode", path: "../JWTDecode.swift")
+        .package(url: "https://github.com/auth0/SimpleKeychain.git", .branch("dynamic")),
+        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift.git", .branch("dynamic"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.ack
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Auth0Dependencies", 
+            dependencies: [
+                "SimpleKeychain",
+                "JWTDecode",
+                .target(name: "Auth0")
+            ], 
+            path: "Package"), // Hack
         .binaryTarget(name: "Auth0", path: "Auth0.xcframework")
     ]
 )
