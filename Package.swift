26248a703dbbae852bca0ac96a10a9043976f904
Rita Zerrizuela <zeta@widcket.com>
2020-09-16 13:23:33 -0300

    Add dependency name to Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(path: "../SimpleKeychain"),
-        .package(path: "../JWTDecode.swift")
+        .package(name: "JWTDecode", path: "../JWTDecode.swift")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
