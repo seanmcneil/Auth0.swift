739a8b874b76dcb2895b24d6c845f51dc55f3f9f
Rita Zerrizuela <zeta@widcket.com>
2020-09-16 13:24:49 -0300

    Remove wrapper target

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Auth0",
-            targets: ["Auth0Dependencies"])
+            targets: ["Auth0"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,15 +20,7 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Auth0Dependencies", 
-            dependencies: [
-                "SimpleKeychain",
-                "JWTDecode",
-                .target(name: "Auth0")
-            ], 
-            path: "Package"), // Hack
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.ack
         .binaryTarget(name: "Auth0", path: "Auth0.xcframework")
     ]
 )
